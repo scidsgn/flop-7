d8615d9bcce39c257bd9e40eebdc924094b45ca4
Kacper Seredyn <kacper17@outlook.com>
2026-02-03 15:37:34 +0100

    feat(client): card alt

--- packages/client/src/card.tsx
+++ packages/client/src/card.tsx
@@ -10,27 +10,53 @@ export const Card = ({ card }: CardProps) => {
             <img
                 className="h-24"
                 src={`/cards/flop-7/number${card.value}.svg`}
+                alt={`Number ${card.value} card`}
             />
         )
     }
     if (card.type === "addModifier") {
-        return <img className="h-24" src={`/cards/flop-7/add${card.add}.svg`} />
+        return (
+            <img
+                className="h-24"
+                src={`/cards/flop-7/add${card.add}.svg`}
+                alt={`Add ${card.add} card`}
+            />
+        )
     }
     if (card.type === "multiplyModifier") {
         return (
             <img
                 className="h-24"
                 src={`/cards/flop-7/multiply${card.multiplier}.svg`}
+                alt={`Multiply by ${card.multiplier} card`}
             />
         )
     }
     if (card.type === "secondChance") {
-        return <img className="h-24" src="/cards/flop-7/secondChance.svg" />
+        return (
+            <img
+                className="h-24"
+                src="/cards/flop-7/secondChance.svg"
+                alt={`Second chance card`}
+            />
+        )
     }
     if (card.type === "freeze") {
-        return <img className="h-24" src="/cards/flop-7/freeze.svg" />
+        return (
+            <img
+                className="h-24"
+                src="/cards/flop-7/freeze.svg"
+                alt={`Freeze card`}
+            />
+        )
     }
     if (card.type === "flopThree") {
-        return <img className="h-24" src="/cards/flop-7/flop3.svg" />
+        return (
+            <img
+                className="h-24"
+                src="/cards/flop-7/flop3.svg"
+                alt={`Flop three card`}
+            />
+        )
     }
 }
